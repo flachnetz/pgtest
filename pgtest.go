338dd3cc0aaa994939a42be3b37d16f135e24066
Oliver Bestmann <obest@esailors.de>
2017-12-27 08:02:40 +0000

    Cleanups

--- pgtest.go
+++ pgtest.go
@@ -2,25 +2,77 @@ package pgtest
 
 import (
 	"errors"
-	"fmt"
-	"github.com/jmoiron/sqlx"
-	"os/exec"
 	"testing"
 	"time"
 
 	"bufio"
+	"database/sql"
 	_ "github.com/lib/pq"
 	"io"
 	"math/rand"
 	"runtime"
 )
 
-type SetupFunc func(db *sqlx.DB) error
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-type TestFunc func(db *sqlx.DB)
+type SetupFunc func(db *sql.DB) error
+
+type TestFunc func(db *sql.DB)
+
+type Instance interface {
+	io.Closer
+	MustConnect() *sql.DB
+}
+
+type Provider interface {
+	Prepare() error
+	Start(t *testing.T) (Instance, error)
+}
+
+type baseInstance struct {
+	uri string
+	t   *testing.T
+}
+
+func (cmd *baseInstance) MustConnect() *sql.DB {
+	for idx := 0; idx < 100; idx++ {
+		time.Sleep(100 * time.Millisecond)
+
+		cmd.t.Log("Trying to connect to postgres database")
+		db, err := sql.Open("postgres", cmd.uri)
+		if err != nil {
+			cmd.t.Log("Could not open connection to database, trying again: ", err)
+			continue
+		}
+
+		if err := db.Ping(); err != nil {
+			cmd.t.Log("Could not ping database, trying again: ", err)
+			continue
+		}
+
+		return db
+	}
+
+	panic(errors.New("could not connect to postgres"))
+}
+
+var InstanceProvider Provider
+
+func init() {
+	if runtime.GOOS == "linux" {
+		InstanceProvider = &pgLinuxProvider{}
+	} else {
+		InstanceProvider = &pgDockerProvider{}
+	}
+}
 
 func WithDatabase(t *testing.T, setup SetupFunc, fn TestFunc) {
-	pg, err := NewPostgresCommand(t)
+	if err := InstanceProvider.Prepare(); err != nil {
+		t.Fatal("Could not prepare postgres provider: ", err)
+		return
+	}
+
+	pg, err := InstanceProvider.Start(t)
 	if err != nil {
 		t.Fatal("Error starting local postgres instance: ", err)
 		return
@@ -28,7 +80,7 @@ func WithDatabase(t *testing.T, setup SetupFunc, fn TestFunc) {
 
 	defer pg.Close()
 
-	db := pg.Connect()
+	db := pg.MustConnect()
 	defer db.Close()
 
 	if err := setup(db); err != nil {
@@ -39,79 +91,8 @@ func WithDatabase(t *testing.T, setup SetupFunc, fn TestFunc) {
 	fn(db)
 }
 
-func NoSetup(*sqlx.DB) error {
+func NoSetup(*sql.DB) error {
 	return nil
-}
-
-type postgresCommand struct {
-	t           *testing.T
-	port        int
-	closeSignal chan bool
-}
-
-// Dockerfile for an optimized postgres container:
-// FROM postgres:10.1-alpine
-// ENV PGDATA=/pg
-// RUN /docker-entrypoint.sh postgres --version
-// USER postgres
-// ENTRYPOINT ["/usr/local/bin/postgres", "-D", "/pg"]
-
-func NewPostgresCommand(t *testing.T) (*postgresCommand, error) {
-	port := rand.Intn(10000) + 20000
-
-	closeSignal := make(chan bool)
-
-	cmd := exec.Command("docker", "run",
-		"--rm", "--name", fmt.Sprintf("postgres-test-%d", port), "-p", fmt.Sprintf("%d:5432", port),
-		"flachnetz/pgtest:10.1-1")
-
-	cmd.Stderr = loggerToWriter("[postgres-out]", t.Log)
-	cmd.Stdout = loggerToWriter("[postgres-err]", t.Log)
-
-	t.Logf("[postgres] Starting new postgres instance on random port %d", port)
-	if err := cmd.Start(); err != nil {
-		return nil, err
-	}
-
-	go func() {
-		cmd.Wait()
-		close(closeSignal)
-	}()
-
-	return &postgresCommand{
-		t:           t,
-		port:        port,
-		closeSignal: closeSignal,
-	}, nil
-}
-
-func (cmd *postgresCommand) Close() {
-	cmd.t.Log("[postgres] Stopping instance now.")
-
-	// run a command to stop and remove the postgres container.
-	exec.Command("docker", "rm", "-fv", fmt.Sprintf("postgres-test-%d", cmd.port)).Run()
-
-	// wait for postgres to stop.
-	<-cmd.closeSignal
-}
-
-func (cmd *postgresCommand) Connect() *sqlx.DB {
-	for idx := 0; idx < 100; idx++ {
-		time.Sleep(250 * time.Millisecond)
-
-		cmd.t.Log("Trying to connect to postgres database")
-		db, err := sqlx.Connect("postgres",
-			fmt.Sprintf("postgres://postgres:postgres@127.0.0.1:%d/postgres?sslmode=disable", cmd.port))
-
-		if err != nil {
-			cmd.t.Log("Could not connect to database, trying again: ", err)
-			continue
-		}
-
-		return db
-	}
-
-	panic(errors.New("could not connect to postgres"))
 }
 
 func loggerToWriter(prefix string, printFunc func(args ...interface{})) io.Writer {
