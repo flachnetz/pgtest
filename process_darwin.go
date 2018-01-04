63d686ae776a3b1155128c35454f2fdd819fcd76
Oliver Bestmann <obest@esailors.de>
2018-01-04 08:23:06 +0000

    Remove wrong comment

--- process_darwin.go
+++ process_darwin.go
@@ -8,7 +8,6 @@ import (
 )
 
 func modifyProcessOnSystem(cmd *exec.Cmd) {
-	// bind the lifetime of the child process to this process.
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Setpgid: true,
 	}
